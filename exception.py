9ddff7b6f542c665a2314ece26eefa79877e9d7a
Vladimir Chegodaev <chegodaev.vl@yandex.ru>
2022-06-21 20:28:36 +0300

    Test commit with error

--- exception.py
+++ exception.py
@@ -1,2 +1,9 @@
+import sentry_sdk
+sentry_sdk.init(
+    dsn="https://45f39db1385648c49f05b49ea04b3df0@o1289354.ingest.sentry.io/6507728",
+    traces_sample_rate=1.0
+)
+
+
 if __name__ == '__main__':
     print(5 / 0)
